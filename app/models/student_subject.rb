b06afaa3a9b33e4b7f8923821a087cdf10a55c96
Mikey Clarke <epicschnozz@gmail.com>
2019-11-04 20:05:01 +1300

    Oops, hadn't expunged all inverse:

--- app/models/student_subject.rb
+++ app/models/student_subject.rb
@@ -1,5 +1,5 @@
 class StudentSubject < ApplicationRecord
 
-  belongs_to :student, inverse: :student_subjects
-  belongs_to :subject, inverse: :student_subjects
+  belongs_to :student, inverse_of: :student_subjects
+  belongs_to :subject, inverse_of: :student_subjects
 end
