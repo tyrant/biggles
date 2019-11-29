635a56cee6f5778980edccf82184ec0d4fb47da4
Mikey Clarke <epicschnozz@gmail.com>
2019-11-29 18:07:50 +1300

    Oops, turns out the two guides Dennis and I have been following to implement JWT aren't quite congruous

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,10 +13,6 @@ class SessionsController < Devise::SessionsController
     request.env['warden-jwt_auth.token']
   end
 
-  def respond_with(resource, _opts = {})
-    render json: resource
-  end
-
   def respond_to_on_destroy
     head :no_content
   end
