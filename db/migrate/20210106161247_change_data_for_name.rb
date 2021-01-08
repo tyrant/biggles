63d17f5f1b9d517c36ee14bc41a806a43347f036
DennisKraaijeveld <dfmkraaijeveld@gmail.com>
2021-01-08 10:19:12 +0100

    latest changes db-migrate file

--- db/migrate/20210106161247_change_data_for_name.rb
+++ db/migrate/20210106161247_change_data_for_name.rb
@@ -1,8 +1,28 @@
 class ChangeDataForName < ActiveRecord::Migration[6.0]
-  def change
+  def up
     change_table :users do |t|
-      t.remove :name
       t.string :first_name
       t.string :last_name
+    end
+    
+    User.find_each do |user|
+      user.first_name = user.name.split(' ')[0]
+      user.last_name = user.name.split(' ')[1]
+      user.save
+    end
+
+    remove_column :users, :name
+  end
+
+  def down
+    add_column :users, :name, :string
+
+    User.find_each do |user|
+      user.name = "#{user.first_name} #{user.last_name}"
+      user.save
+    end
+
+    remove_column :users, :first_name
+    remove_column :users, :last_name
   end
 end
